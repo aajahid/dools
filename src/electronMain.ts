8fa03d68c5c576faf3a351080fdeff46314e249a
Abdullah Al Jahid <abdullah.al.jahid@gmail.com>
2021-06-11 16:36:40 +0600

    Styling for the dools search bar

--- src/electronMain.ts
+++ src/electronMain.ts
@@ -5,9 +5,11 @@ function createAndReturnWindow () {
     let display = screen.getPrimaryDisplay();
     const win = new BrowserWindow({
         width:  500,
-        height: 80,
-        frame:  true,
+        height: 70,
+        frame:  false,
         skipTaskbar: true,
+        resizable: false,
+        transparent: true,
         x: (display.size.width / 2) - 250,
         y: (display.size.height /2 ) - 400,
         webPreferences: {
@@ -29,6 +31,13 @@ app.whenReady().then(() => {
             win.show();
     })
 
+    win.on('blur', () => {
+        if (BrowserWindow.getAllWindows().length > 0) {
+            if (win.isVisible()) {
+                win.hide()
+            }
+        }
+    })
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
